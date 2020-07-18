0090cbdf5f62b3a45fb0360172508e9eb4fcf792
rkalidh <51086337+rkalidh@users.noreply.github.com>
2020-07-18 12:45:52 +0530

    Update newresource.tf

--- newresource.tf
+++ newresource.tf
@@ -1,5 +1,10 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
     region = "ca-central-1"  
+    access_key = var.aws_access_key
+   secret_key = var.aws_secret_key
     
 }
 
